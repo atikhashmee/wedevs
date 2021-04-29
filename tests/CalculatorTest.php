b8415be123262a143641041bf842b46eaee3c92d
Atik Bin Hashmee <atikhashmee6235@gmail.com>
2021-04-29 19:42:32 +0600

    first test case writing

---
+++ tests/CalculatorTest.php
@@ -0,0 +1,12 @@
+<?php
+namespace Tests;
+
+class CalculatorTest extends \PHPUnit\Framework\TestCase {
+    public function testAdd()
+    {
+        $calculator = new \App\Calculator;
+        $calculator->setOperands([5,20]);
+        $this->assertEquals(25, $calculator->add());
+    }
+}
+
